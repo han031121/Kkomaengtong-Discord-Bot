468d6eb623f438560578549cb06291d725f060a5
han031121 <han031121@gmail.com>
2026-08-13 14:15:20 +0900

    fix(wordle): fix wordle server record container

--- src/commands/wordle/panel.ts
+++ src/commands/wordle/panel.ts
@@ -97,14 +97,7 @@ export function createAllWordleRecordsContainer(
 ): ContainerBuilder {
     return new ContainerBuilder()
         .setAccentColor(Colors.Blurple)
-        .addTextDisplayComponents(
-            new TextDisplayBuilder().setContent(
-                [
-                    "### 현재 서버 Wordle 기록 순위",
-                    `-# 항목별 최대 5위까지 표기`,
-                ].join("\n"),
-            ),
-        )
+        .addTextDisplayComponents(new TextDisplayBuilder().setContent("### 현재 서버 Wordle 기록 순위"))
         .addSeparatorComponents(createSeparator())
         .addTextDisplayComponents(
             new TextDisplayBuilder().setContent(
@@ -122,7 +115,8 @@ export function createAllWordleRecordsContainer(
                     ),
                 ].join("\n"),
             ),
-        );
+        )
+        .addTextDisplayComponents(new TextDisplayBuilder().setContent(`-# 항목별 최대 5위까지 표기`));
 }
 
 function getStatusText(game: WordleGame): string {
